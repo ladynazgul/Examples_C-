7f92524e31c2c08d2fbe931dbcb6b9b477347a43
ladynazgool <ladynazgool@gmail.com>
2022-08-13 02:42:40 +0200

    random added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 15;
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
+Console.WriteLine(numberB);
 Console.WriteLine(result);
