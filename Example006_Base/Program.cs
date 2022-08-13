a5222dfbc553a33521ce395fa95ab86507d16d89
ladynazgool <ladynazgool@gmail.com>
2022-08-13 13:18:34 +0200

    Base project added

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1869460;
+int b = 94857;
+int c = 64849453;
+int d = 8534587;
+int e = 435;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
